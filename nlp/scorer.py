f1a4fb9bd8d475fcb4ef14680cf44353ce5a5704
Aliesapour79 <51175760+Aliesapour79@users.noreply.github.com>
2026-06-17 16:24:03 +0330

    Update scorer.py

--- nlp/scorer.py
+++ nlp/scorer.py
@@ -1,1 +1,15 @@
+from sentence_transformers import SentenceTransformer, util
+import numpy as np
 
+model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+
+USER_PROFILE = """
+Python, Embedded Systems, ESP32, IoT, Machine Learning, PCB Design, C++
+"""
+
+user_vec = model.encode(USER_PROFILE, convert_to_tensor=True)
+
+
+def semantic_score(text):
+    job_vec = model.encode(text, convert_to_tensor=True)
+    return util.cos_sim(user_vec, job_vec).item() * 100
