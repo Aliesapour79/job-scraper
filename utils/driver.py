fe4fb96995ed61f1bff5f37920ad71c8bfd918a8
Aliesapour79 <51175760+Aliesapour79@users.noreply.github.com>
2026-06-23 15:55:06 +0000

    headless=new

--- utils/driver.py
+++ utils/driver.py
@@ -12,7 +12,7 @@ import platform
 def setup_driver():
     """Setup Chrome driver with automatic driver management"""
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
